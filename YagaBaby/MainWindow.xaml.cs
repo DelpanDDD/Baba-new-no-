693e4a8f32bab630736aacf84e411fd2042ab0d0
dozopravka <dozopravka@example.com>
2023-06-05 21:37:58 +0300

    prod by dozo with TROYAN

---
+++ YagaBaby/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YagaBaby
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void info(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new info());
+        }
+
+        private void siz(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new siz());
+        }
+
+        private void DangerZone(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new dangerzone());
+        }
+
+        private void details(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new details());
+        }
+
+        private void size(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new size());
+        }
+
+        private void filter(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new filter());
+        }
+
+        private void control(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new control());
+        }
+
+        private void qrcode(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new qr());
+        }
+
+        private void dozo(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+    }
+}
---
+++ YagaBaby/siz.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YagaBaby
+{
+    /// <summary>
+    /// Логика взаимодействия для siz.xaml
+    /// </summary>
+    public partial class siz : Page
+    {
+        public siz()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/aborinada/semestr-5/blob/main/skrin.md");
+        }
+    }
+}
